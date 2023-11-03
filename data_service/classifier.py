db016c83c157d47dd44fd5f92cc46c75933492e5
Viktor Fendesak <79378770+vfcapital@users.noreply.github.com>
2023-11-03 23:29:45 +0100

    Data service (#2)
    * added article loader
    
    * added ai classification of articles

---
+++ data_service/classifier.py
@@ -0,0 +1,67 @@
+import yaml
+import openai
+import json
+from pathlib import Path
+from tqdm import tqdm
+
+
+def get_article_classification(messages, secrets):
+        # Initialize OpenAI
+    openai.organization = secrets["ORG_ID"]
+    openai.api_key = secrets["OPEN_AI_KEY"]
+    response = openai.ChatCompletion.create(
+        model="gpt-4", 
+        messages=messages,
+        temperature=0.7,
+    )
+    return json.loads(response.choices[0].message['content'].strip())
+
+
+def main():
+    # Load articles
+    storage_path = Path(__file__).parent / "storage"
+    default_path = Path(__file__).parent / "default"
+    article_ids = [file.stem for file in storage_path.glob("*") if file.is_file()]
+
+    # Load API key
+    with open(Path(__file__).parent / ".secrets.yaml", "r") as f:
+        secrets = yaml.load(f, Loader=yaml.FullLoader)
+
+    # Load system message
+    with open(default_path / "system_message.txt", "r") as f:
+        system_message = f.read()
+
+    # Load tags
+    with open(default_path / "tags.txt", "r") as f:
+        tags = [line.strip() for line in f]
+
+    update_counter = 0
+
+    for _id in tqdm(article_ids, ncols=60):
+        with open(storage_path / (_id + ".json"), "r") as f:
+            article_versions = json.load(f)
+
+        article_text = article_versions["versions"]["0"]["article_text"]
+
+        messages = [
+            {"role": "system", "content": system_message},
+            {
+                "role": "user", 
+                "content": (
+                    f"Select the tags from this list: {tags} "
+                    f"and extract the key message of this news article: {article_text}"
+                )
+            }
+        ]
+
+        if "key_message" not in article_versions.keys():
+            # Extract response
+            article_classifications = get_article_classification(messages, secrets)
+            article_versions.update(article_classifications)
+            with open(storage_path / (_id + ".json"), "w") as f:
+                json.dump(article_versions, f)
+            update_counter += 1
+
+
+if __name__ == "__main__":
+    main()
---
+++ data_service/loader.py
@@ -0,0 +1,68 @@
+import json
+import sys
+import uuid
+from pathlib import Path
+
+from newspaper import Article
+
+
+def create_new_version(article):
+    print("New version stored.")
+    return {
+        "title": article.title,
+        "publish_date": article.publish_date.strftime("%Y-%m-%dT%H:%M:%S"),
+        "article_text": article.text,
+    }
+
+
+def articles_different(old, new):
+    if old != new:
+        return True
+    else:
+        return False
+
+
+def main(URL):
+    # URL to ID
+    _id = str(uuid.uuid5(uuid.NAMESPACE_URL, URL))
+
+    # Fetch article data
+    article = Article(URL)
+    article.download()
+    article.parse()
+
+    # Check if article exists in storage
+    storage_path = Path(__file__).parent / "storage"
+    existing_article_ids = [
+        file.stem for file in storage_path.glob("*") if file.is_file()
+    ]
+    file_path = storage_path / (_id + ".json")
+
+    # Update versions
+    if _id in existing_article_ids:
+        with open(file_path, "r") as f:
+            article_versions = json.load(f)
+        prev_version = sorted(list(article_versions["versions"].keys()))[-1]
+        prev_text = article_versions["versions"][prev_version]["article_text"]
+        if articles_different(prev_text, article.text):
+            version = int(prev_version) + 1
+            article_versions["versions"][str(version)] = create_new_version(article)
+        else:
+            print("Already up-to-date.")
+    else:
+        article_versions = {}
+        version = 0
+        article_versions["url"] = URL
+        article_versions["versions"] = {version: create_new_version(article)}
+
+    # Store updated versions
+    with open(file_path, "w") as f:
+        json.dump(article_versions, f)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        URL = input("Please enter your URL: \n")
+    else:
+        URL = sys.argv[1]
+    main(URL)
