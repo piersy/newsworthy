de4d59c3c4ccf35e0300008ebe6820476e150030
Piers Powlesland <pierspowlesland@gmail.com>
2023-11-04 12:21:11 +0000

    Add deploy script

---
+++ Contracts/scripts/deploy.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const unlockTime = currentTimestampInSeconds + 60;
+
+  // const lockedAmount = ethers.parseEther("0.001");
+
+  const newsworthy = await ethers.deployContract("NewsWorthy");
+  await newsworthy.waitForDeployment();
+
+  console.log(`${newsworthy.target}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
